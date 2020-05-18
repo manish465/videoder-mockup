1b7662c940bf3400f16f7be9f177e135d578fdbc
unknown <ms5978307@gmail.com>
2020-05-18 14:25:47 -0700

    added naem and path to the links

---
+++ src/Data/LinksData.js
@@ -0,0 +1,14 @@
+export default data = [
+    { name: "Youtube", img: "../Assets/Links/download.png" },
+    { name: "9Anime", img: "../Assets/Links/9anime.png" },
+    { name: "9GAG", img: "../Assets/Links/9gag.png" },
+    { name: "AudioBoom", img: "../Assets/Links/audio_boom.png" },
+    { name: "Dailymotion", img: "../Assets/Links/dailymotion.png" },
+    { name: "Facebook", img: "../Assets/Links/facebook.png" },
+    { name: "Instagram", img: "../Assets/Links/instagram.png" },
+    { name: "Ozee", img: "../Assets/Links/ozee.png" },
+    { name: "Soundcloud", img: "../Assets/Links/soundcloud.png" },
+    { name: "Tictok", img: "../Assets/Links/tiktok.png" },
+    { name: "Twitter", img: "../Assets/Links/twitter.png" },
+    { name: "Voot", img: "../Assets/Links/voot.png" },
+];
