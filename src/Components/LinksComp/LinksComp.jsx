c432f9b815749c9a2b5a679d05e7cae3ebd48a3d
unknown <ms5978307@gmail.com>
2020-05-18 15:45:57 -0700

    added external links

--- src/Components/LinksComp/LinksComp.jsx
+++ src/Components/LinksComp/LinksComp.jsx
@@ -1,16 +1,40 @@
 import React from "react";
-import { Paper } from "@material-ui/core";
+import { Paper, Avatar, Grid, IconButton, Typography } from "@material-ui/core";
+import data from "../../Data/LinksData";
 
 const LinksComp = () => {
+    const newData = data.slice(0, 5);
+
+    console.log(newData);
+
     return (
         <Paper
             style={{
                 margin: "30px",
                 height: "220px",
                 backgroundColor: "#171717",
+                justifyContent: "center",
             }}
             variant='outlined'>
-            hfas
+            <Grid
+                container
+                style={{ marginTop: "20px" }}
+                justify='space-around'
+                direction='row'>
+                {newData.map((el, key) => (
+                    <div>
+                        <IconButton>
+                            <Avatar
+                                style={{ width: "80px", height: "80px" }}
+                                src={el.img}
+                            />
+                        </IconButton>
+                        <Typography style={{ color: "#ffffff" }}>
+                            {el.name}
+                        </Typography>
+                    </div>
+                ))}
+            </Grid>
         </Paper>
     );
 };
--- src/Data/LinksData.js
+++ src/Data/LinksData.js
@@ -1,14 +1,29 @@
-export default data = [
-    { name: "Youtube", img: "../Assets/Links/download.png" },
-    { name: "9Anime", img: "../Assets/Links/9anime.png" },
-    { name: "9GAG", img: "../Assets/Links/9gag.png" },
-    { name: "AudioBoom", img: "../Assets/Links/audio_boom.png" },
-    { name: "Dailymotion", img: "../Assets/Links/dailymotion.png" },
-    { name: "Facebook", img: "../Assets/Links/facebook.png" },
-    { name: "Instagram", img: "../Assets/Links/instagram.png" },
-    { name: "Ozee", img: "../Assets/Links/ozee.png" },
-    { name: "Soundcloud", img: "../Assets/Links/soundcloud.png" },
-    { name: "Tictok", img: "../Assets/Links/tiktok.png" },
-    { name: "Twitter", img: "../Assets/Links/twitter.png" },
-    { name: "Voot", img: "../Assets/Links/voot.png" },
+import img1 from "../Assets/Links/download.png";
+import img2 from "../Assets/Links/facebook.png";
+import img3 from "../Assets/Links/instagram.png";
+import img4 from "../Assets/Links/twitter.png";
+import img5 from "../Assets/Links/tiktok.png";
+import img6 from "../Assets/Links/9anime.png";
+import img7 from "../Assets/Links/9gag.png";
+import img8 from "../Assets/Links/audio_boom.png";
+import img9 from "../Assets/Links/dailymotion.png";
+import img10 from "../Assets/Links/ozee.png";
+import img11 from "../Assets/Links/soundcloud.png";
+import img12 from "../Assets/Links/voot.png";
+
+const data = [
+    { name: "Youtube", img: img1 },
+    { name: "Facebook", img: img2 },
+    { name: "Instagram", img: img3 },
+    { name: "Twitter", img: img4 },
+    { name: "Tictok", img: img5 },
+    { name: "9Anime", img: img6 },
+    { name: "9GAG", img: img7 },
+    { name: "AudioBoom", img: img8 },
+    { name: "Dailymotion", img: img9 },
+    { name: "Ozee", img: img10 },
+    { name: "Soundcloud", img: img11 },
+    { name: "Voot", img: img12 },
 ];
+
+export default data;
